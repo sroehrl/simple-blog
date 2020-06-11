f8cc939aa0f19fd638de7210afffe8fc1fd52f0c
sroehrl <stefan.roehrl@gmail.com>
2020-06-11 13:36:04 -0400

    fix user-blog list

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once 'config.php';
 
 $articles = [];
-$articles = $client->getArticleList(['author' => 'neoan']);
+$articles = $client->getArticleList(0,100,$_ENV['BLUA_BLUE_USERNAME']);
 
 
 echo \Neoan3\Apps\Template::embraceFromFile('template/main.html', [
